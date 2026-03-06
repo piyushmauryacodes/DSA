ed48f2d0e246613151c9d16d984939a57edc05be
piyushmaurya0823-crypto <piyushmaurya0823@gmail.com>
2026-03-06 23:46:48 +0530

    added a hashing question

---
+++ hashing/Counting_frequencies_of_array_Elements/index.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countFrequencies(self,nums):
+        hash_list=[0]*len(nums)
+        arr=[]
+        cnt=0
+        for num in nums:
+            hash_list[num]+=1
+        
+        for num in hash_list:
+            ar=[]
+            ar.append(([cnt,num]))
+            cnt+=1
+            arr+=ar
+        return arr
+
+
+sol=Solution()
+n=int(input("Enter the size of array: "))
+arr=[]
+while n>=1:
+    num=int(input("Enter your element no. "))
+    arr.append(num)
+    n-=1
+
+
+print(sol.countFrequencies(arr))

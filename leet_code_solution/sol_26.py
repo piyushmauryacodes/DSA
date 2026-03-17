c2a0e49de3ab954d83f626393a9a8f7b2341c55c
piyushmaurya0823-crypto <piyushmaurya0823@gmail.com>
2026-03-17 19:19:49 +0530

    little change in Sol_26 on 17-03-2026 in leetcode

--- leet_code_solution/sol_26.py
+++ leet_code_solution/sol_26.py
@@ -1,13 +1,12 @@
 class Solution(object):
     def removeDuplicates(self, nums):
-        print(len(nums))
-        print(nums)
         i=0
         for j in range(1,len(nums)):
             if nums[i]!=nums[j]:
                 i+=1
                 nums[i]=nums[j]             
-        return len(nums)
+        print(nums,len(nums))
+        return nums[:i+1]
     
 
 sol=Solution()
